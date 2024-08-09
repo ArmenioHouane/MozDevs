c41e08c19ce4757823c00ea0949f0bf74963e610
ArmenioHouane <166590270+ArmenioHouane@users.noreply.github.com>
2024-08-09 13:54:01 +0200

    up

---
+++ mozdevs/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+import { Error } from "./pages/404";
+import Card from "./pages/cards";
+
+export default function Home() {
+  return (
+    <>
+    <Card />
+
+      
+    </>
+  );
+}
